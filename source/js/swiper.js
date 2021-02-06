0914ac615d51f93f9b020d1497995a91b3b22a7a
dosjcq <nagibin.artyom1999@gmail.com>
2021-02-06 15:32:36 +0300

    add swiper final

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,6 +1,6 @@
  var swiper = new Swiper('.swiper-container', {
       slidesPerView: 'auto',
-      spaceBetween: 30,
+      spaceBetween: 56,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
