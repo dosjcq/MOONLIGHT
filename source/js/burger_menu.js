293054f3d84ad1337bec37b147da265513ce742f
dosjcq <nagibin.artyom1999@gmail.com>
2021-02-08 14:47:02 +0300

    added burger final

--- source/js/burger_menu.js
+++ source/js/burger_menu.js
@@ -1,6 +1,6 @@
-$(".menu-burger, .menu-items").on("click", function () {
-	$(".menu-bg, .menu-items, .menu-burger").toggleClass("fs");
-	$(".menu-burger").text() == "☰"
-		? $(".menu-burger").text("✕")
-		: $(".menu-burger").text("☰");
+$(document).ready(function() {
+    $('.header_burger').click(function(event) {
+        $('.header_burger,.header_menu,.header_list').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
